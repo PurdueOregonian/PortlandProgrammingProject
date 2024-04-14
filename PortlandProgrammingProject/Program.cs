47861a3b17d6a20497ce1196185bd91ad93af863
PurdueOregonian <williamliang451@gmail.com>
2024-04-14 16:11:02 -0700

    this should work idk what's wrong

--- PortlandProgrammingProject/Program.cs
+++ PortlandProgrammingProject/Program.cs
@@ -1,10 +1,12 @@
 using PortlandProgrammingProject.Components;
+using Blazored.Modal;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddBlazoredModal();
 
 var app = builder.Build();
 
