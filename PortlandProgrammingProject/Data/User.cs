3743591fca0358daf75295792db398e8abcbf2db
PurdueOregonian <williamliang451@gmail.com>
2024-04-20 15:46:26 -0700

    user stuff

---
+++ PortlandProgrammingProject/Data/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PortlandProgrammingProject.Data
+{
+    public struct User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FavoriteFood { get; set; }
+    }
+}
