f6e2cf749f03f6ae12f9ac90951d657c3d18e73b
PurdueOregonian <williamliang451@gmail.com>
2024-04-24 22:26:59 -0700

    copying the auth service from my other project

---
+++ PortlandProgrammingProject/Services/AuthService.cs
@@ -0,0 +1,48 @@
+﻿using Friends5___Backend.Authentication;
+using Microsoft.AspNetCore.Identity;
+
+namespace Friends5___Backend.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IdentityResult> RegisterUser(LoginInfo loginInfo)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = loginInfo.Username
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(identityUser, loginInfo.Password);
+                return result;
+            }
+            catch (Exception)
+            {
+                return IdentityResult.Failed(new IdentityError { Code = "CustomErrorCode", Description = "An error occurred while creating the user." });
+            }
+        }
+        public async Task<LoginResult> Login(LoginInfo loginInfo)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(loginInfo.Username);
+                if (user == null)
+                {
+                    return new LoginResult { Success = false };
+                }
+                return new LoginResult { Success = await _userManager.CheckPasswordAsync(user, loginInfo.Password) };
+            }
+            catch(Exception ex)
+            {
+                return new LoginResult { Success = false, ErrorMessage = ex.Message };
+            }
+        }
+    }
+}
---
+++ PortlandProgrammingProject/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Friends5___Backend.Authentication;
+using Microsoft.AspNetCore.Identity;
+
+namespace Friends5___Backend.Services
+{
+    public interface IAuthService
+    {
+        public Task<IdentityResult> RegisterUser(LoginInfo loginInfo);
+        public Task<LoginResult> Login(LoginInfo loginInfo);
+    }
+}
